04db3839b7c99a0e2d70f58cc2f16a76910b912f
AdamCodd <49641859+AdamCodd@users.noreply.github.com>
2023-08-26 00:56:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,27 +4,29 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="hyperdb-python",
-    version="0.1.4",
-    author="John Dagdelen",
-    author_email="jjdagdelen@gmail.com",
+    name="local-hyperDB",
+    version="0.1.5",
+    author="Adam F",
+    author_email="49641859+AdamCodd@users.noreply.github.com",
     description="A hyper-fast local vector database for use with LLM Agents.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jdagdelen/hyperdb",
+    url="https://github.com/AdamCodd/local-hyperDB/",
     packages=find_packages(),
     install_requires=[
         "numpy",
-        "openai",
+        "pytest",
+        "transformers",
+        "fast_sentence_transformers"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     python_requires='>=3.8',
 )
