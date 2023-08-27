Fix vector normalization

hyperdb/ranking_algorithm.py
@@ -7,7 +7,9 @@ def get_norm_vector(vector):
         return vector / (np.linalg.norm(vector) + epsilon)
     else:
         norms = np.linalg.norm(vector, axis=1)[:, np.newaxis] + epsilon
-        return vector / norms
+        normalized_vector = np.where(norms > epsilon, vector / norms, vector)
+        return normalized_vector
+
 
 def dot_product(vectors, query_vector):
     similarities = np.dot(vectors, query_vector.T)
@@ -19,7 +21,6 @@ def cosine_similarity(vectors, query_vector):
     similarities = np.dot(norm_vectors, norm_query_vector.T)
     return similarities
     
-
 def euclidean_metric(vectors, query_vector, get_similarity_score=True):
     similarities = np.linalg.norm(vectors - query_vector, axis=1)
     if get_similarity_score:
@@ -51,8 +52,10 @@ def hyper_SVM_ranking_algorithm_sort(vectors, query_vector, top_k=5, metric=cosi
 def custom_ranking_algorithm_sort(vectors, query_vector, timestamps, top_k=5, metric=cosine_similarity, recency_bias=0):
     """HyperSVMRanking altered to take into account a recency_bias and favour more recent documents (recent memories)"""
     similarities = metric(vectors, query_vector)
+    
     # Flatten the similarities array to 1-D if it's not already
-    similarities = similarities.flatten()
+    similarities = similarities.flatten()   
+
     try:
         # Attempt to convert timestamps to floats
         float_timestamps = [float(timestamp) if timestamp is not None else 0.0 for timestamp in timestamps]
@@ -68,6 +71,7 @@ def custom_ranking_algorithm_sort(vectors, query_vector, timestamps, top_k=5, me
         recency_scores = [0] * len(similarities)  # set recency_scores to 0 when not used
     # Combine the similarities and the recency scores
     combined_scores = [similarity + recency for similarity, recency in zip(similarities, recency_scores)]
+
     # Efficiently fetch top-k indices
     if len(combined_scores) > 0:
         actual_top_k = min(top_k, len(combined_scores))  # Limit top_k to the length of combined_scores
@@ -76,5 +80,5 @@ def custom_ranking_algorithm_sort(vectors, query_vector, timestamps, top_k=5, me
         top_indices = top_indices[np.argsort(-np.array(combined_scores)[top_indices])]
     else:
         return [], [], []
-
+    
     return top_indices, np.array(combined_scores)[top_indices], np.array(similarities)[top_indices]